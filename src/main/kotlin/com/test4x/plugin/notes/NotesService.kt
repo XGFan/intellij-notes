24971949d5607e175906912be8ec944787d12a12
Guofan Xu <xuguofan@live.com>
2019-12-13 23:40:10 +0800

    使用java file api代替intellij VFS 😊

--- src/main/kotlin/com/test4x/plugin/notes/NotesService.kt
+++ src/main/kotlin/com/test4x/plugin/notes/NotesService.kt
@@ -1,10 +1,12 @@
 package com.test4x.plugin.notes
 
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.ServiceManager
-import com.intellij.openapi.fileEditor.impl.LoadTextUtil
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.util.io.createFile
+import com.intellij.util.io.exists
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 import java.util.concurrent.ConcurrentHashMap
 
 class NotesService(project: Project) {
@@ -17,19 +19,15 @@ class NotesService(project: Project) {
 
     private var lastTimestamp = System.currentTimeMillis() - 50 * 1000
     private val cache = ConcurrentHashMap<String, String>()
-    private val localFile: VirtualFile
+    private val file: Path
 
     init {
         val projectFileSystem = project.projectFile?.parent?.parent!!
-        this.localFile = projectFileSystem.findChild(NoteFile)
-            ?: ApplicationManager.getApplication().runWriteAction<VirtualFile> {
-                projectFileSystem.createChildData(
-                    this::class.java,
-                    NoteFile
-                )
-            }
-        val loadText = LoadTextUtil.loadText(this.localFile)
-        loadText.split("\n").forEach {
+        file = Paths.get(projectFileSystem.path).resolve(NoteFile)
+        if (!file.exists()) {
+            file.createFile()
+        }
+        Files.readAllLines(file).forEach {
             val index = it.indexOf("=")
             if (index != -1) {
                 cache[it.substring(0, index)] = it.substring(index + 1)
@@ -53,12 +51,10 @@ class NotesService(project: Project) {
     }
 
     private fun saveFile() {
-        if (System.currentTimeMillis() - lastTimestamp >= 60 * 1000) {
+        if (System.currentTimeMillis() - lastTimestamp >= 5 * 1000) {
             val toByteArray =
                 cache.map { "${it.key}=${it.value}" }.joinToString(separator = "\n").toByteArray(Charsets.UTF_8)
-            ApplicationManager.getApplication().runWriteAction {
-                localFile.setBinaryContent(toByteArray)
-            }
+            Files.write(file, toByteArray)
             this.lastTimestamp = System.currentTimeMillis()
         }
     }
